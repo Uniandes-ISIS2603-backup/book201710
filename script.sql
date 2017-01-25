78f70e1eb4b59912933edf83d1309b81122593bc
Diego F. Ruiz S <yego23@gmail.com>
2017-01-25 17:18:39 -0500

    Script para BookEntity

---
+++ script.sql
@@ -0,0 +1,5 @@
+insert into BookEntity (id, name, isbn, image, description, publishDate) values (2, 'Voyatouch', '549388042-3', 'http://dummyimage.com/121x224.png/ff4444/ffffff', 'Supplement R Tympanic Membrane with Synth Sub, Via Opening', '8/20/1996');
+insert into BookEntity (id, name, isbn, image, description, publishDate) values (3, 'Solarbreeze', '507119915-7', 'http://dummyimage.com/150x116.png/ff4444/ffffff', 'Occlusion of Right Femoral Artery, Percutaneous Approach', '2/19/2014');
+insert into BookEntity (id, name, isbn, image, description, publishDate) values (4, 'Wrapsafe', '279453624-9', 'http://dummyimage.com/192x240.png/5fa2dd/ffffff', 'Removal of Spacer from T-lum Jt, Perc Approach', '4/7/1998');
+insert into BookEntity (id, name, isbn, image, description, publishDate) values (5, 'Quo Lux', '744706866-7', 'http://dummyimage.com/242x250.jpg/ff4444/ffffff', 'Reposition Left Femoral Shaft, Perc Endo Approach', '10/10/1998');
+insert into BookEntity (id, name, isbn, image, description, publishDate) values (6, 'Asoka', '260760424-9', 'http://dummyimage.com/158x100.png/dddddd/000000', 'Supplement Lower Artery with Autol Sub, Perc Approach', '5/9/2013');
